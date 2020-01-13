60ecea8f687c95dcf908436eaa1673f7d7fb37d5
Denis Warsow <stdedjub@googlemail.com>
2020-01-13 15:12:53 +0100

    fix create accessControl Classes

--- src/generator/default/access.php
+++ src/generator/default/access.php
@@ -1,14 +1,25 @@
-<?php echo '<?php' ?>
+<?php
+/* class starts here */
+echo "<?php\n"; 
+?>
+<?php if(strlen($namespaceName) > 0) : ?>
+namespace <?= $namespaceName ?> ; 
+<?php endif; ?> 
 
-namespace <?= $namespace ?>
+use yii\filters\AccessControl;
 
-class <?php echo $className ?> {
-
-
-	public static function check(){
-		<?= $rules =>
-	}
-
-
-
+class <?= $className ?> extends AccessControl
+{
+    public $rules = [ 
+<?php foreach($rules as $access) : ?>
+[
+<?php foreach($access as $key=>$value) : ?> 
+<?php if($key == 'allow') : ?> '<?= $key ?>' => <?= ($value==1?'true':'false') ?>, 
+<?php else: ?>
+<?php if(is_array($value)) : ?>
+'<?=$key?>' => [ <?php foreach($value as $singleValue): ?> '<?= $singleValue ?>', <?php endforeach; ?> ],
+<?php endif; ?> 
+<?php endif; ?>
+<?php endforeach; ?> ],
+<?php endforeach; ?> ];
 }
